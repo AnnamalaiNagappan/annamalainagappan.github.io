53d3f3fc83b45ce4e885f682bee6a428fc0dfe77
Annamalai Nagappan <annamalai.nagappan@gramener.com>
2015-04-08 12:08:11 +0530

    ENH:jumbotron css modified

--- js/gallery_app.js
+++ js/gallery_app.js
@@ -1,7 +1,8 @@
 angular.module('displayApp', [])
   .controller('galleryController', function($scope) {
     $scope.gallery_items = [
-      {title: 'Time Relativity', url: 'angular-apps/time_relativity/index.html'},
-      {title: 'Sort | Filter Tables', url: 'angular-apps/sort_filter_tables/index.html'},
+      {title: 'Time Relativity', url: 'angular-apps/time_relativity/index.html', category: 'Angular'},
+      {title: 'Sort | Filter Tables', url: 'angular-apps/sort_filter_tables/index.html', category: 'Angular'},
+      {title: 'Invoice', url: 'invoice.html', category: 'Bootstrap'},
     ];
   });
