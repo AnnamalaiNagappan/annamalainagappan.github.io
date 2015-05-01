a2dc4602ba447aa8ed5179d831b5e9233bbb4af8
unknown <annamalai.nagappan@gramener.com>
2015-05-01 15:47:45 +0530

    Meteor app added

--- js/gallery_app.js
+++ js/gallery_app.js
@@ -4,5 +4,6 @@ angular.module('displayApp', [])
       {title: 'Time Relativity', url: 'angular-apps/time_relativity/index.html', category: 'Angular'},
       {title: 'Sort | Filter Tables', url: 'angular-apps/sort_filter_tables/index.html', category: 'Angular'},
       {title: 'Invoice', url: 'invoice.html', category: 'Bootstrap'},
+      {title: 'Data Sharing', url: 'http://datashare.meteor.com/', category: 'Meteor'},
     ];
   });
